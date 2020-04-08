5a44f1770d2dd1a4db8abf01ad52243eeeedc578
hamzakhalil3343 <hamza.khalil3343@gmail.com>
2020-04-08 22:54:27 +0500

    Props and Event Handler are added

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,8 @@
 import React,{Component} from 'react';
-import { Media } from 'reactstrap';
+
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'reactstrap';
+
+
 class Menu extends Component{
     constructor(props){
                                 
@@ -8,72 +11,63 @@ class Menu extends Component{
 
         super(props);
         this.state = {
-            dishes: [
-                {
-                  id: 0,
-                  name:'Uthappizza',
-                  image: 'assets/images/uthappizza.png',
-                  category: 'mains',
-                  label:'Hot',
-                  price:'4.99',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-               {
-                  id: 1,
-                  name:'Zucchipakoda',
-                  image: 'assets/images/zucchipakoda.png',
-                  category: 'appetizer',
-                  label:'',
-                  price:'1.99',
-                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-               {
-                  id: 2,
-                  name:'Vadonut',
-                  image: 'assets/images/vadonut.png',
-                  category: 'appetizer',
-                  label:'New',
-                  price:'1.99',
-                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-               {
-                  id: 3,
-                  name:'ElaiCheese Cake',
-                  image: 'assets/images/elaicheesecake.png',
-                  category: 'dessert',
-                  label:'',
-                  price:'2.99',
-                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-               ],
+          selectedDish: null
+           
         };
+      }
 
 
 
+      onDishSelect(dish) {
+        this.setState({ selectedDish: dish});
     }
-    render(){
-        const menu = this.state.dishes.map((dish) => {
-            return (
-              <div key={dish.id} className="col-12 mt-5">
-                <Media tag="li">
-                  <Media left middle>
-                      <Media object src={dish.image} alt={dish.name} />
-                  </Media>
-                  <Media body className="ml-5">
-                    <Media heading>{dish.name}</Media>
-                    <p>{dish.description}</p>
-                  </Media>
-                </Media>
-              </div>
+
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
             );
-        });
-
-        return (
-          <div className="container">
-            <div className="row">
-              <Media list>
-                  {menu}
-              </Media>
-            </div>
-          </div>
-        );
-
+        else
+            return(
+                <div></div>
+            );
     }
+
+
+
+    render() {
+      const menu = this.props.dishes.map((dish) => {
+          return (
+            <div  className="col-12 col-md-5 m-1">
+              <Card key={dish.id}
+                onClick={() => this.onDishSelect(dish)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+              </Card>
+            </div>
+          );
+      });
+
+      return (
+          <div className="container">
+              <div className="row">
+                  {menu}
+              </div>
+              <div className="row">
+                <div  className="col-12 col-md-5 m-1">
+                  {this.renderDish(this.state.selectedDish)}
+                </div>
+              </div>
+          </div>
+      );
+  }
 }
 export default Menu;

4f52081f4c5aa1c7593c7fed8d61e2401142d228
hamzakhalil3343 <hamza.khalil3343@gmail.com>
2020-04-17 18:47:01 +0500

    UnControlled form  in React

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -28,6 +28,7 @@ import { Breadcrumb, BreadcrumbItem,
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
         
     }
 
